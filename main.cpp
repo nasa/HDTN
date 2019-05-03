83ed4189291e1205d0560032bd336348d36a0903
Tomko <btomko@ndc.nasa.gov>
2019-05-03 15:25:11 -0400

    start testing a map

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <string>
 #include "MemoryManagerTree.h"
-
-
+#include <map>
+#include <boost/integer.hpp>
+#include <stdint.h>
 
 
 
 int main() {
-	
-	std::cout << MemoryManagerTree::UnitTest() << "\n";
+	// 64^4 = 16,777,216
+	// 16777216 * 64 = 1,073,741,824
+	// 4e12 / 8192 = 488,281,250
+	std::map<boost::uint64_t, boost::uint32_t> myMap;
+	MemoryManagerTree t;
+	t.SetupTree();
+	getchar();
+	for (boost::uint64_t i = 0; i < 16777216 * 64; ++i) {
+		const boost::uint32_t segmentId = t.GetAndSetFirstFreeSegmentId();
+		myMap[i] = segmentId;
+		if (i % 16777216 == 0) {
+			std::cout << "pause at " << i << "\n";
+			getchar();
+		}
+	}
+
+	//std::cout << MemoryManagerTree::UnitTest() << "\n";
 	return 0;
 }
