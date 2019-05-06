6c5dada34c32e65ef670b77587583af3e7809476
Tomko <btomko@ndc.nasa.gov>
2019-05-06 11:59:07 -0400

    test map implementation of bundle storage manager

--- BundleStorageManager.cpp
+++ BundleStorageManager.cpp
@@ -1,25 +1,61 @@
 #include "BundleStorageManager.h"
+#include "MemoryManagerTreeArray.h"
 #include <iostream>
 #include <string>
 
 
 
 void BundleStorageManager::AddLink(const std::string & linkName) {
+#ifdef USE_VECTOR_CIRCULAR_BUFFER
 	priority_vec_t priorityVec(NUMBER_OF_PRIORITIES);
 	for (unsigned int i = 0; i < NUMBER_OF_PRIORITIES; ++i) {
 		priorityVec[i] = expiration_circular_buf_t(NUMBER_OF_EXPIRATIONS);
 	}
 	m_destMap[linkName] = std::move(priorityVec);
+#else
+	m_destMap[linkName] = priority_vec_t(NUMBER_OF_PRIORITIES);
+#endif
+}
+
+void BundleStorageManager::StoreBundle(const std::string & linkName, const unsigned int priorityIndex, const abs_expiration_t absExpiration, const segment_id_t segmentId) {
+#ifdef USE_VECTOR_CIRCULAR_BUFFER
+
+#else
+	priority_vec_t & priorityVec = m_destMap[linkName];
+	expiration_map_t & expirationMap = priorityVec[priorityIndex];
+	segment_id_vec_t & segmentIdVec = expirationMap[absExpiration];
+	segmentIdVec.push_back(segmentId);
+	////segmentIdVec.shrink_to_fit();
+#endif
 }
 
 bool BundleStorageManager::UnitTest() {
+	static const std::string DEST_LINKS[10] = { "a1", "a2","a3", "a4", "a5", "a6", "a7", "a8", "a9", "b1" };
 	BundleStorageManager bsm;
-	for (char i = 0; i < 10; ++i) {
-		std::string key("mykey");
-		key += 'a' + i;		
-		std::cout << "about to add key " << key << "\n";
-		getchar();
-		bsm.AddLink(key); //results in about 12MB per link
+	MemoryManagerTreeArray mmt;
+	mmt.SetupTree();
+	for (int i = 0; i < 10; ++i) {
+		bsm.AddLink(DEST_LINKS[i]);
+	}
+
+	unsigned int linkId = 0;
+	unsigned int priorityIndex = 0;
+	abs_expiration_t absExpiration = 0;
+
+	for (boost::uint32_t i = 0; i < 16777216 * 64; ++i) {
+		if (i % 16777216 == 0) {
+			std::cout << "about to push, i=" << i << "\n";
+			//getchar();
+		}
+		const boost::uint32_t segmentId = mmt.GetAndSetFirstFreeSegmentId();
+		if (segmentId != i) {
+			std::cout << "error " << segmentId << " " << i << "\n";
+			return false;
+		}
+		bsm.StoreBundle(DEST_LINKS[linkId], priorityIndex, absExpiration + 100000, segmentId);
+		linkId = (linkId + 1) % 10;
+		priorityIndex = (priorityIndex + 1) % 3;
+		absExpiration = (absExpiration + 1) % NUMBER_OF_EXPIRATIONS;
 	}
 
 	return true;
--- BundleStorageManager.h
+++ BundleStorageManager.h
@@ -7,11 +7,21 @@
 #include <vector>
 #include <string>
 
+//#define USE_VECTOR_CIRCULAR_BUFFER 1
+#ifdef USE_VECTOR_CIRCULAR_BUFFER
 typedef boost::uint32_t segment_id_t;
 typedef std::vector<segment_id_t> segment_id_vec_t;
 typedef std::vector<segment_id_vec_t> expiration_circular_buf_t;
 typedef std::vector<expiration_circular_buf_t> priority_vec_t;
+typedef std::map<std::string, priority_vec_t> destination_map_t; //results in about 12MB per link
+#else
+typedef boost::uint32_t segment_id_t;
+typedef boost::uint64_t abs_expiration_t;
+typedef std::vector<segment_id_t> segment_id_vec_t;
+typedef std::map<abs_expiration_t, segment_id_vec_t> expiration_map_t;
+typedef std::vector<expiration_map_t> priority_vec_t;
 typedef std::map<std::string, priority_vec_t> destination_map_t;
+#endif
 
 //two days
 #define NUMBER_OF_EXPIRATIONS (86400*2)
@@ -21,6 +31,7 @@ class BundleStorageManager {
 public:
 
 	void AddLink(const std::string & linkName);
+	void StoreBundle(const std::string & linkName, const unsigned int priorityIndex, const abs_expiration_t absExpiration, const segment_id_t segmentId);
 	
 	static bool UnitTest();
 
