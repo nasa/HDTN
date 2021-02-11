86e716ebc0e82bfaa5f6997f33073139f7ef92c8
Brian <brian.j.tomko@nasa.gov>
2021-02-10 23:07:30 -0500

    begin implementation of tcpcl into BpsinkAsync

--- common/bpcodec/apps/bpsink/BpSinkAsync.cpp
+++ common/bpcodec/apps/bpsink/BpSinkAsync.cpp
@@ -43,13 +43,24 @@ BpSinkAsync::BpSinkAsync(uint16_t port, bool useTcp) :
     m_rxPortUdpOrTcp(port),
     m_useTcp(useTcp),
     m_udpSocket(m_ioService),
-    m_tcpAcceptor(m_ioService, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 12346)),
+    m_tcpAcceptor(m_ioService, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)),
+    m_noKeepAlivePacketReceivedTimer(m_ioService),
+    m_needToSendKeepAliveMessageTimer(m_ioService),
     m_circularIndexBuffer(BP_MSG_NBUF),
     m_udpReceiveBuffersCbVec(BP_MSG_NBUF),
     m_remoteEndpointsCbVec(BP_MSG_NBUF),
     m_udpReceiveBytesTransferredCbVec(BP_MSG_NBUF),
     m_running(false)
 {
+    if(useTcp) {
+        m_tcpcl.SetContactHeaderReadCallback(boost::bind(&BpSinkAsync::ContactHeaderCallback, this, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3));
+        m_tcpcl.SetDataSegmentContentsReadCallback(boost::bind(&BpSinkAsync::DataSegmentCallback, this, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3));
+        m_tcpcl.SetAckSegmentReadCallback(boost::bind(&BpSinkAsync::AckCallback, this, boost::placeholders::_1));
+        m_tcpcl.SetBundleRefusalCallback(boost::bind(&BpSinkAsync::BundleRefusalCallback, this, boost::placeholders::_1));
+        m_tcpcl.SetNextBundleLengthCallback(boost::bind(&BpSinkAsync::NextBundleLengthCallback, this, boost::placeholders::_1));
+        m_tcpcl.SetKeepAliveCallback(boost::bind(&BpSinkAsync::KeepAliveCallback, this));
+        m_tcpcl.SetShutdownMessageCallback(boost::bind(&BpSinkAsync::ShutdownCallback, this, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3, boost::placeholders::_4));
+    }
 }
 
 BpSinkAsync::~BpSinkAsync() {
@@ -259,6 +270,7 @@ void BpSinkAsync::StartTcpAccept() {
 
 void BpSinkAsync::HandleTcpAccept(boost::shared_ptr<boost::asio::ip::tcp::socket> newTcpSocketPtr, const boost::system::error_code& error) {
     if (!error) {
+        ShutdownAndCloseTcpSocket(); //also reset tcpcl states
         std::cout << "tcp connection: " << newTcpSocketPtr->remote_endpoint().address() << ":" << newTcpSocketPtr->remote_endpoint().port() << "\n";
         m_tcpSocketPtr = newTcpSocketPtr;
         StartTcpReceive();
@@ -293,7 +305,7 @@ void BpSinkAsync::HandleTcpReceiveSome(const boost::system::error_code & error, 
     }
     else if (error == boost::asio::error::eof) {
         std::cout << "Tcp connection closed cleanly by peer" << std::endl;
-        m_tcpSocketPtr = boost::shared_ptr<boost::asio::ip::tcp::socket>(); //delete the socket
+        ShutdownAndCloseTcpSocket();
     }
     else if (error != boost::asio::error::operation_aborted) {
         std::cerr << "Error in BpSinkAsync::HandleTcpReceiveSome: " << error.message() << std::endl;
@@ -315,13 +327,180 @@ void BpSinkAsync::PopCbThreadFunc() {
             //thread is now unblocked, and the lock is reacquired by invoking lock.lock()
             continue;
         }
-        Process(m_udpReceiveBuffersCbVec[consumeIndex], m_udpReceiveBytesTransferredCbVec[consumeIndex]);
+        if(m_useTcp) {
+            m_tcpcl.HandleReceivedChars(m_udpReceiveBuffersCbVec[consumeIndex].data(), m_udpReceiveBytesTransferredCbVec[consumeIndex]);
+        }
+        else {
+            Process(m_udpReceiveBuffersCbVec[consumeIndex], m_udpReceiveBytesTransferredCbVec[consumeIndex]);
+        }
         //std::cout << "2" << std::endl;
         m_circularIndexBuffer.CommitRead();
     }
 
     std::cout << "BpSinkAsync Circular buffer reader thread exiting\n";
 
+}
+
+void BpSinkAsync::HandleTcpSend(boost::shared_ptr<std::vector<boost::uint8_t> > dataSentPtr, const boost::system::error_code& error, std::size_t bytes_transferred, bool closeSocket) {
+    if(closeSocket) {
+        ShutdownAndCloseTcpSocket();
+    }
+}
+
+void BpSinkAsync::ContactHeaderCallback(CONTACT_HEADER_FLAGS flags, uint16_t keepAliveIntervalSeconds, const std::string & localEid) {
+    m_contactHeaderFlags = flags;
+    //The keepalive_interval parameter is set to the minimum value from
+    //both contact headers.  If one or both contact headers contains the
+    //value zero, then the keepalive feature (described in Section 5.6)
+    //is disabled.
+    m_keepAliveIntervalSeconds = keepAliveIntervalSeconds;
+    m_localEid = localEid;
+    std::cout << "received contact header from " << m_localEid << std::endl;
+
+    //Since BpSink was waiting for a contact header, it just got one.  Now it's time to reply with a contact header
+    //use the same keepalive interval
+    if(m_tcpSocketPtr) {
+        boost::shared_ptr<std::vector<uint8_t> > contactHeaderPtr = boost::make_shared<std::vector<uint8_t> >();
+        Tcpcl::GenerateContactHeader(*contactHeaderPtr, static_cast<CONTACT_HEADER_FLAGS>(0), keepAliveIntervalSeconds, "bpsink");
+        boost::asio::async_write(*m_tcpSocketPtr, boost::asio::buffer(*contactHeaderPtr),
+                                         boost::bind(&BpSinkAsync::HandleTcpSend, this, contactHeaderPtr,
+                                                     boost::asio::placeholders::error,
+                                                     boost::asio::placeholders::bytes_transferred, false));
+        if(m_keepAliveIntervalSeconds) { //non-zero
+            std::cout << "using " << keepAliveIntervalSeconds << " seconds for keepalive\n";
+
+            // * 2 =>
+            //If no message (KEEPALIVE or other) has been received for at least
+            //twice the keepalive_interval, then either party MAY terminate the
+            //session by transmitting a one-byte SHUTDOWN message (as described in
+            //Table 2) and by closing the TCP connection.
+            m_noKeepAlivePacketReceivedTimer.expires_from_now(boost::posix_time::seconds(m_keepAliveIntervalSeconds * 2));
+            m_noKeepAlivePacketReceivedTimer.async_wait(boost::bind(&BpSinkAsync::OnNoKeepAlivePacketReceived_TimerExpired, this, boost::asio::placeholders::error));
+
+
+            m_needToSendKeepAliveMessageTimer.expires_from_now(boost::posix_time::seconds(m_keepAliveIntervalSeconds));
+            m_needToSendKeepAliveMessageTimer.async_wait(boost::bind(&BpSinkAsync::OnNeedToSendKeepAliveMessage_TimerExpired, this, boost::asio::placeholders::error));
+        }
+    }
+}
+
+void BpSinkAsync::DataSegmentCallback(boost::shared_ptr<std::vector<uint8_t> > dataSegmentDataSharedPtr, bool isStartFlag, bool isEndFlag) {
+
+    uint32_t bytesToAck = 0;
+    if(isStartFlag && isEndFlag) { //optimization for whole (non-fragmented) data
+        Process(*dataSegmentDataSharedPtr, dataSegmentDataSharedPtr->size());
+        bytesToAck = static_cast<uint32_t>(dataSegmentDataSharedPtr->size());
+    }
+    else {
+        if (isStartFlag) {
+            m_fragmentedBundleRxConcat.resize(0);
+        }
+        m_fragmentedBundleRxConcat.insert(m_fragmentedBundleRxConcat.end(), dataSegmentDataSharedPtr->begin(), dataSegmentDataSharedPtr->end()); //concatenate
+        bytesToAck = static_cast<uint32_t>(m_fragmentedBundleRxConcat.size());
+        if(isEndFlag) { //fragmentation complete
+            Process(m_fragmentedBundleRxConcat, m_fragmentedBundleRxConcat.size());
+        }
+    }
+    //send ack
+    if((static_cast<unsigned int>(CONTACT_HEADER_FLAGS::REQUEST_ACK_OF_BUNDLE_SEGMENTS)) & (static_cast<unsigned int>(m_contactHeaderFlags))) {
+        if(m_tcpSocketPtr) {
+            boost::shared_ptr<std::vector<uint8_t> > ackPtr = boost::make_shared<std::vector<uint8_t> >();
+            Tcpcl::GenerateAckSegment(*ackPtr, bytesToAck);
+            boost::asio::async_write(*m_tcpSocketPtr, boost::asio::buffer(*ackPtr),
+                                             boost::bind(&BpSinkAsync::HandleTcpSend, this, ackPtr,
+                                                         boost::asio::placeholders::error,
+                                                         boost::asio::placeholders::bytes_transferred, false));
+        }
+    }
+}
+
+void BpSinkAsync::AckCallback(uint32_t totalBytesAcknowledged) {
+    std::cout << "bpsink should never enter AckCallback" << std::endl;
+}
+
+void BpSinkAsync::BundleRefusalCallback(BUNDLE_REFUSAL_CODES refusalCode) {
+    std::cout << "bpsink should never enter BundleRefusalCallback" << std::endl;
+}
+
+void BpSinkAsync::NextBundleLengthCallback(uint32_t nextBundleLength) {
+    std::cout << "next bundle length: " << nextBundleLength << std::endl;
+}
+
+void BpSinkAsync::KeepAliveCallback() {
+    std::cout << "received keepalive packet\n";
+    // * 2 =>
+    //If no message (KEEPALIVE or other) has been received for at least
+    //twice the keepalive_interval, then either party MAY terminate the
+    //session by transmitting a one-byte SHUTDOWN message (as described in
+    //Table 2) and by closing the TCP connection.
+    m_noKeepAlivePacketReceivedTimer.expires_from_now(boost::posix_time::seconds(m_keepAliveIntervalSeconds * 2)); //cancels active timer with cancel flag in callback
+    m_noKeepAlivePacketReceivedTimer.async_wait(boost::bind(&BpSinkAsync::OnNoKeepAlivePacketReceived_TimerExpired, this, boost::asio::placeholders::error));
+}
+
+void BpSinkAsync::ShutdownCallback(bool hasReasonCode, SHUTDOWN_REASON_CODES shutdownReasonCode,
+                                         bool hasReconnectionDelay, uint32_t reconnectionDelaySeconds)
+{
+    std::cout << "remote has requested shutdown\n";
+    if(hasReasonCode) {
+        std::cout << "reason for shutdown: "
+                  << ((shutdownReasonCode == SHUTDOWN_REASON_CODES::BUSY) ? "busy" :
+                     (shutdownReasonCode == SHUTDOWN_REASON_CODES::IDLE_TIMEOUT) ? "idle timeout" :
+                     (shutdownReasonCode == SHUTDOWN_REASON_CODES::VERSION_MISMATCH) ? "version mismatch" :  "unassigned")   << std::endl;
+    }
+    if(hasReconnectionDelay) {
+        std::cout << "requested reconnection delay: " << reconnectionDelaySeconds << " seconds" << std::endl;
+    }
+    ShutdownAndCloseTcpSocket();
+}
+
+void BpSinkAsync::OnNoKeepAlivePacketReceived_TimerExpired(const boost::system::error_code& e) {
+    if (e != boost::asio::error::operation_aborted) {
+        // Timer was not cancelled, take necessary action.
+        if(m_tcpSocketPtr) {
+            std::cout << "error: tcp keepalive timed out.. closing socket\n";
+            boost::shared_ptr<std::vector<uint8_t> > shutdownPtr = boost::make_shared<std::vector<uint8_t> >();
+            Tcpcl::GenerateShutdownMessage(*shutdownPtr, true, SHUTDOWN_REASON_CODES::IDLE_TIMEOUT, false, 0);
+            boost::asio::async_write(*m_tcpSocketPtr, boost::asio::buffer(*shutdownPtr),
+                                             boost::bind(&BpSinkAsync::HandleTcpSend, this, shutdownPtr,
+                                                         boost::asio::placeholders::error,
+                                                         boost::asio::placeholders::bytes_transferred, true)); //true => shutdown after data sent
+        }
+
+    }
+    else {
+        //std::cout << "timer cancelled\n";
+    }
+}
+
+void BpSinkAsync::OnNeedToSendKeepAliveMessage_TimerExpired(const boost::system::error_code& e) {
+    if (e != boost::asio::error::operation_aborted) {
+        // Timer was not cancelled, take necessary action.
+        //SEND KEEPALIVE PACKET
+        if(m_tcpSocketPtr) {
+            boost::shared_ptr<std::vector<uint8_t> > keepAlivePtr = boost::make_shared<std::vector<uint8_t> >();
+            Tcpcl::GenerateKeepAliveMessage(*keepAlivePtr);
+            boost::asio::async_write(*m_tcpSocketPtr, boost::asio::buffer(*keepAlivePtr),
+                                             boost::bind(&BpSinkAsync::HandleTcpSend, this, keepAlivePtr,
+                                                         boost::asio::placeholders::error,
+                                                         boost::asio::placeholders::bytes_transferred, false));
+
+            m_needToSendKeepAliveMessageTimer.expires_from_now(boost::posix_time::seconds(m_keepAliveIntervalSeconds));
+            m_needToSendKeepAliveMessageTimer.async_wait(boost::bind(&BpSinkAsync::OnNeedToSendKeepAliveMessage_TimerExpired, this, boost::asio::placeholders::error));
+        }
+    }
+    else {
+        //std::cout << "timer cancelled\n";
+    }
+}
+
+void BpSinkAsync::ShutdownAndCloseTcpSocket() {
+    if(m_tcpSocketPtr) {
+        std::cout << "shutting down connection\n";
+        m_tcpSocketPtr = boost::shared_ptr<boost::asio::ip::tcp::socket>();
+    }
+    m_needToSendKeepAliveMessageTimer.cancel();
+    m_noKeepAlivePacketReceivedTimer.cancel();
+    m_tcpcl.InitRx(); //reset states
 }
 
 }  // namespace hdtn
--- common/bpcodec/apps/bpsink/include/BpSinkAsync.h
+++ common/bpcodec/apps/bpsink/include/BpSinkAsync.h
@@ -12,7 +12,7 @@
 #include "CircularIndexBufferSingleProducerSingleConsumerConfigurable.h"
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
-
+#include "Tcpcl.h"
 
 namespace hdtn {
 
@@ -36,6 +36,26 @@ private:
     void HandleTcpAccept(boost::shared_ptr<boost::asio::ip::tcp::socket> newTcpSocketPtr, const boost::system::error_code& error);
     void StartTcpReceive();
     void HandleTcpReceiveSome(const boost::system::error_code & error, std::size_t bytesTransferred, unsigned int writeIndex);
+    void HandleTcpSend(boost::shared_ptr<std::vector<boost::uint8_t> > dataSentPtr, const boost::system::error_code& error, std::size_t bytes_transferred, bool closeSocket);
+    void OnNoKeepAlivePacketReceived_TimerExpired(const boost::system::error_code& e);
+    void OnNeedToSendKeepAliveMessage_TimerExpired(const boost::system::error_code& e);
+    void ShutdownAndCloseTcpSocket();
+
+    //tcpcl received data callback functions
+    void ContactHeaderCallback(CONTACT_HEADER_FLAGS flags, uint16_t keepAliveIntervalSeconds, const std::string & localEid);
+    void DataSegmentCallback(boost::shared_ptr<std::vector<uint8_t> > dataSegmentDataSharedPtr, bool isStartFlag, bool isEndFlag);
+    void AckCallback(uint32_t totalBytesAcknowledged);
+    void BundleRefusalCallback(BUNDLE_REFUSAL_CODES refusalCode);
+    void NextBundleLengthCallback(uint32_t nextBundleLength);
+    void KeepAliveCallback();
+    void ShutdownCallback(bool hasReasonCode, SHUTDOWN_REASON_CODES shutdownReasonCode,
+                                             bool hasReconnectionDelay, uint32_t reconnectionDelaySeconds);
+
+    //tcpcl vars
+    CONTACT_HEADER_FLAGS m_contactHeaderFlags;
+    uint16_t m_keepAliveIntervalSeconds;
+    std::string m_localEid;
+    std::vector<uint8_t> m_fragmentedBundleRxConcat;
 
 public:
     uint32_t m_batch;
@@ -52,11 +72,14 @@ public:
 private:
     const uint16_t m_rxPortUdpOrTcp;
     const bool m_useTcp;
+    Tcpcl m_tcpcl;
 
     int m_type;
     boost::asio::io_service m_ioService;
     boost::asio::ip::udp::socket m_udpSocket;
     boost::asio::ip::tcp::acceptor m_tcpAcceptor;
+    boost::asio::deadline_timer m_noKeepAlivePacketReceivedTimer;
+    boost::asio::deadline_timer m_needToSendKeepAliveMessageTimer;
     boost::shared_ptr<boost::asio::ip::tcp::socket> m_tcpSocketPtr;
     CircularIndexBufferSingleProducerSingleConsumerConfigurable m_circularIndexBuffer;
     std::vector<std::vector<boost::uint8_t> > m_udpReceiveBuffersCbVec;
