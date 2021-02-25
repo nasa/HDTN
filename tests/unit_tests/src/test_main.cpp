b73031f5528f3df9601562a77fae96b3adf1001b
Tomko <btomko@ndc.nasa.gov>
2021-02-25 14:28:29 -0500

    limit tests that run on windows

--- tests/unit_tests/src/test_main.cpp
+++ tests/unit_tests/src/test_main.cpp
@@ -1,5 +1,14 @@
 #define BOOST_TEST_MODULE HtdnUnitTestsModule
 
+//todo: better cmake solution to detect if we are using boost static or shared libs... assume for now
+//      that shared libs will be used on linux and static libs will be used on windows.
+#ifndef _WIN32
+#define BOOST_TEST_DYN_LINK
+#endif
+
+//#define BOOST_TEST_NO_MAIN 1
+
+
 #include <boost/test/unit_test.hpp>
 
 
