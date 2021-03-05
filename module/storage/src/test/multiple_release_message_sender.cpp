1788a9dbf79025da38c60bdc8bcbae55b90991d9
jfollo <jeffrey.c.follo@nasa.gov>
2021-03-05 15:26:22 -0500

    added environment path for json file

--- module/storage/src/test/multiple_release_message_sender.cpp
+++ module/storage/src/test/multiple_release_message_sender.cpp
@@ -8,11 +8,17 @@
 #include "message.hpp"
 #include "paths.hpp"
 #include "reg.hpp"
+#include "Environment.h"
 
 
 int main(int argc, char *argv[]) {
 
+    std::string jsonFileName = (Environment::GetPathHdtnSourceRoot() /
+      "module/storage/src/test/releaseMessages1.json").string();
+
+
     std::cout << "Running muliple_release_message_sender.  " << std::endl << std::flush;
+    std::cout << "jsonFileName:  " << jsonFileName << std::endl << std::flush;
 
     return 0;
 }
