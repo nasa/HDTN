eacc889311cd435e3742d2e65ef6c02510f7b511
jfollo <jeffrey.c.follo@nasa.gov>
2021-03-05 14:29:48 -0500

    Added to make system

---
+++ module/storage/src/test/multiple_release_message_sender.cpp
@@ -0,0 +1,18 @@
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
+#include <cstdlib>
+#include <iostream>
+
+#include "message.hpp"
+#include "paths.hpp"
+#include "reg.hpp"
+
+
+int main(int argc, char *argv[]) {
+
+    std::cout << "Running muliple_release_message_sender.  " << std::endl << std::flush;
+
+    return 0;
+}
