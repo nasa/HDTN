97fde6cee8db7837ba94d0f9fe6247ea113dacac
jfollo <jeffrey.c.follo@nasa.gov>
2021-03-07 11:46:39 -0500

    Have multiple deadline timers working now.

--- module/storage/src/test/multiple_release_message_sender.cpp
+++ module/storage/src/test/multiple_release_message_sender.cpp
@@ -13,9 +13,9 @@
 
 //#include <boost/make_shared.hpp>
 #include <boost/foreach.hpp>
+#include <boost/date_time.hpp>
 
-
-
+typedef boost::shared_ptr<boost::asio::deadline_timer> SmartDeadlineTimer;
 
 struct ReleaseMessageEvent_t {
     int id;
@@ -23,6 +23,16 @@ struct ReleaseMessageEvent_t {
     std::string message;
 };
 typedef std::vector<ReleaseMessageEvent_t> ReleaseMessageEventVector_t;
+
+void print(const boost::system::error_code&, int id) {
+
+  // Get current system time
+  boost::posix_time::ptime timeLocal = boost::posix_time::second_clock::local_time();
+  std::cout <<  "id: " << id << " , expiry time: " << timeLocal << std::endl << std::flush;
+
+
+}
+
 
 
 int main(int argc, char *argv[]) {
@@ -52,6 +62,26 @@ int main(int argc, char *argv[]) {
         std::cout << ", delay: " << releaseMessageEventVector[i].delay;
         std::cout << ", message: " << releaseMessageEventVector[i].message << std::endl << std::flush;
     }
+
+    boost::asio::io_service ioService;
+
+    std::vector<SmartDeadlineTimer> vectorTimers;
+    for(int i=0; i<releaseMessageEventVector.size(); ++i) {
+        SmartDeadlineTimer dt(new boost::asio::deadline_timer(ioService));
+        dt->expires_from_now(boost::posix_time::seconds(releaseMessageEventVector[i].delay));
+       // dt->async_wait(boost::bind(&print,releaseMessageEventVector[i].id));
+
+
+        dt->async_wait(boost::bind(print,boost::asio::placeholders::error, releaseMessageEventVector[i].id));
+
+        vectorTimers.push_back(dt);
+    }
+
+    std::cout << "before run: " << std::endl << std::flush;
+
+    ioService.run();
+
+    std::cout << "after run: " << std::endl << std::flush;
 
     return 0;
 }
