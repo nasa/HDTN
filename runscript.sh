35d0580e5acb3ad620fc7a18c22cd34f612cad9e
nadia <nadia.kortas@nasa.gov>
2021-05-10 18:30:48 -0400

    Updated the script to run HDTN as the previous one wasn't working Updated the README file

--- runscript.sh
+++ runscript.sh
@@ -1,22 +1,22 @@
-cd ~/hdtn/common/regsvr
+cd $HDTN_SOURCE_ROOT/common/regsvr
 `python3 main.py` &
 sleep 3
-cd ~/hdtn/build/common/bpcodec/apps
+cd $HDTN_SOURCE_ROOT/build/common/bpcodec/apps
 ./bpsink-async "--use-tcpcl" "--port=4558" &
 sleep 3
-cd ~/hdtn/build/module/egress
+cd $HDTN_SOURCE_ROOT/build/module/egress
 ./hdtn-egress-async "--use-tcpcl" "--port1=0" "--port2=4558" &
 sleep 3
-cd ~/hdtn/build/module/ingress 
+cd $HDTN_SOURCE_ROOT/build/module/ingress 
 ./hdtn-ingress "--always-send-to-storage" & 
 sleep 3
-cd ~/hdtn/build/module/storage
+cd $HDTN_SOURCE_ROOT/build/module/storage
 ./hdtn-release-message-sender "--release-message-type=start" "--flow-id=2" "--delay-before-send=14" &
 sleep 1
-cd ~/hdtn/build/module/storage
+cd $HDTN_SOURCE_ROOT/build/module/storage
 ./hdtn-storage "--storage-config-json-file=$HDTN_SOURCE_ROOT/module/storage/storage-brian/unit_tests/storageConfigRelativePaths.json" &
 sleep 3
-cd ~/hdtn/build/common/bpcodec/apps
+cd $HDTN_SOURCE_ROOT/build/common/bpcodec/apps
 ./bpgen-async "--bundle-rate=100" "--use-tcpcl" "--flow-id=2" "--duration=5" &
 sleep 8
 
