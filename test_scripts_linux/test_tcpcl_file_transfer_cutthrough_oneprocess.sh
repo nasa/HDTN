1bdbe95f6162546ff1a71b9dad4230ee5deada35
nadia kortas <nadia.kortas@nasa.gov>
2021-09-21 17:03:27 -0400

    scripts

--- test_scripts_linux/test_tcpcl_file_transfer_cutthrough_oneprocess.sh
+++ test_scripts_linux/test_tcpcl_file_transfer_cutthrough_oneprocess.sh
@@ -14,7 +14,7 @@ sleep 3
 
 # BP Receive File
 ./build/common/bpcodec/apps/bpreceivefile --save-directory=received --my-uri-eid=ipn:2.1 --inducts-config-file=$sink_config &
-bpsink1_PID=$!
+bpreceive_PID=$!
 sleep 3
 
 # HDTN one process
@@ -24,16 +24,16 @@ sleep 6
 
 # BP Send File 
 ./build/common/bpcodec/apps/bpsendfile --max-bundle-size-bytes=4000000 --file-or-folder-path=test.txt --my-uri-eid=ipn:1.1 --dest-uri-eid=ipn:2.1 --outducts-config-file=$gen_config &
-bpgen2_PID=$!
+bpsend_PID=$!
 sleep 8
 
 # cleanup
 sleep 30
-echo "\nkilling bpgen..." && kill -2 $bpgen2_PID
+echo "\nkilling bp send file..." && kill -2 $bpsend_PID
 sleep 2
-echo "\nkilling egress..." && kill -2 $one_process_PID
+echo "\nkilling HDTN one process ..." && kill -2 $one_process_PID
 sleep 2
-echo "\nkilling bpsink1..." && kill -2 $bpsink1_PID
+echo "\nkilling bp receive file..." && kill -2 $bpreceive_PID
 sleep 2
 echo "\nkilling registration server..." && pkill -9 -f main.py
 
