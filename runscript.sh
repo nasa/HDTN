2a33a7fee73f1324c90dba086599e642d7498263
nadia kortas <nadia.kortas@nasa.gov>
2021-10-19 11:22:27 -0400

    Update HDTN runscript to work with nextHopEndpointId changes

--- runscript.sh
+++ runscript.sh
@@ -14,7 +14,7 @@ python3 ./common/regsvr/main.py &
 sleep 3
 
 # bpsink1
-./build/common/bpcodec/apps/bpsink-async --my-uri-eid=ipn:3.1 --inducts-config-file=$sink1_config &
+./build/common/bpcodec/apps/bpsink-async --my-uri-eid=ipn:1.1 --inducts-config-file=$sink1_config &
 bpsink1_PID=$!
 sleep 3
 
@@ -29,7 +29,7 @@ egress_PID=$!
 sleep 3
 
 #Scheduler
-./build/module/scheduler/hdtn-scheduler --contact-plan-file=contactPlanIpn2.1.json --hdtn-config-file=$hdtn_config &
+./build/module/scheduler/hdtn-scheduler --contact-plan-file=contactPlan.json --hdtn-config-file=$hdtn_config &
 scheduler_PID=$!
 sleep 1
 
@@ -44,12 +44,12 @@ storage_PID=$!
 sleep 3
 
 # bpgen1
-./build/common/bpcodec/apps/bpgen-async --bundle-rate=100 --my-uri-eid=ipn:1.1 --dest-uri-eid=ipn:3.1 --duration=40 --outducts-config-file=$gen_config &
+./build/common/bpcodec/apps/bpgen-async --bundle-rate=100 --my-uri-eid=ipn:101.1 --dest-uri-eid=ipn:1.1 --duration=40 --outducts-config-file=$gen_config &
 bpgen1_PID=$!
 sleep 1
 
 #bpgen2
-./build/common/bpcodec/apps/bpgen-async --bundle-rate=100 --my-uri-eid=ipn:1.1 --dest-uri-eid=ipn:2.1 --duration=40 --outducts-config-file=$gen_config &
+./build/common/bpcodec/apps/bpgen-async --bundle-rate=100 --my-uri-eid=ipn:102.1 --dest-uri-eid=ipn:2.1 --duration=40 --outducts-config-file=$gen_config &
 bpgen2_PID=$!
 sleep 8
 
