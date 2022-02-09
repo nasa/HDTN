058cadee64e77b4c4480235f7660e35dc2787eca
btomko <btomko@GRLWL2020050782>
2022-02-09 08:26:30 -0500

    split bpv6.cpp out into Bpv6CbhePrimaryBlock.cpp and Bpv6CanonicalBlock.cpp

--- common/bpcodec/src/codec/Bpv6CbhePrimaryBlock.cpp
+++ common/bpcodec/src/codec/Bpv6CbhePrimaryBlock.cpp
@@ -263,57 +263,6 @@ uint64_t Bpv6CbhePrimaryBlock::SerializeBpv6(uint8_t * serialization) const {
     return serialization - serializationBase;
 }
 
-void bpv6_canonical_block::SetZero() {
-    flags = 0;
-    length = 0;
-    type = 0;
-}
-
-uint32_t bpv6_canonical_block::bpv6_canonical_block_decode(const char* buffer, const size_t offset, const size_t bufsz) {
-    uint64_t index = offset;
-    uint8_t sdnvSize;
-    type = buffer[index++];
-
-    const uint8_t flag8bit = buffer[index];
-    if (flag8bit <= 127) {
-        flags = flag8bit;
-        ++index;
-    }
-    else {
-        flags = SdnvDecodeU64((const uint8_t *)&buffer[index], &sdnvSize);
-        if (sdnvSize == 0) {
-            return 0; //return 0 on failure
-        }
-        index += sdnvSize;
-    }
-
-    length = SdnvDecodeU64((const uint8_t *)&buffer[index], &sdnvSize);
-    if (sdnvSize == 0) {
-        return 0; //return 0 on failure
-    }
-    index += sdnvSize;
-
-    return static_cast<uint32_t>(index - offset);
-}
-
-uint32_t bpv6_canonical_block::bpv6_canonical_block_encode(char* buffer, const size_t offset, const size_t bufsz) const {
-    uint64_t index = offset;
-    uint64_t sdnvSize;
-    buffer[index++] = type;
-
-    if (flags <= 127) {
-        buffer[index++] = static_cast<uint8_t>(flags);
-    }
-    else {
-        sdnvSize = SdnvEncodeU64((uint8_t *)&buffer[index], flags);
-        index += sdnvSize;
-    }
-
-    sdnvSize = SdnvEncodeU64((uint8_t *)&buffer[index], length);
-    index += sdnvSize;
-
-    return static_cast<uint32_t>(index - offset);
-}
 
 void Bpv6CbhePrimaryBlock::bpv6_primary_block_print() const {
     printf("BPv6 / Primary block (%" PRIu64 " bytes)\n", m_blockLength);
@@ -360,66 +309,6 @@ void Bpv6CbhePrimaryBlock::bpv6_primary_block_print() const {
     printf("Lifetime: %" PRIu64 "\n", m_lifetimeSeconds);
 }
 
-void bpv6_canonical_block::bpv6_canonical_block_print() const {
-    printf("Canonical block [type %u]\n", type);
-    switch(type) {
-        case BPV6_BLOCKTYPE_AUTHENTICATION:
-            printf("> Authentication block\n");
-            break;
-        case BPV6_BLOCKTYPE_EXTENSION_SECURITY:
-            printf("> Extension security block\n");
-            break;
-        case BPV6_BLOCKTYPE_INTEGRITY:
-            printf("> Integrity block\n");
-            break;
-        case BPV6_BLOCKTYPE_METADATA_EXTENSION:
-            printf("> Metadata block\n");
-            break;
-        case BPV6_BLOCKTYPE_PAYLOAD:
-            printf("> Payload block\n");
-            break;
-        case BPV6_BLOCKTYPE_PAYLOAD_CONF:
-            printf("> Payload confidentiality block\n");
-            break;
-        case BPV6_BLOCKTYPE_PREV_HOP_INSERTION:
-            printf("> Previous hop insertion block\n");
-            break;
-        case BPV6_BLOCKTYPE_CUST_TRANSFER_EXT:
-       	    printf("> ACS custody transfer extension block (CTEB)\n");
-            break;
-        case BPV6_BLOCKTYPE_BPLIB_BIB:
-   	    printf("> Bplib bundle integrity block (BIB)\n");
-            break;
-        case BPV6_BLOCKTYPE_BUNDLE_AGE:
-            printf("> Bundle age extension (BAE)\n");
-            break;
-        default:
-            printf("> Unknown block type\n");
-            break;
-    }
-    bpv6_block_flags_print();
-    printf("Block length: %" PRIu64 " bytes\n", length);
-}
-
-void bpv6_canonical_block::bpv6_block_flags_print() const {
-    printf("Flags: 0x%" PRIx64 "\n", flags);
-    if (flags & BPV6_BLOCKFLAG_LAST_BLOCK) {
-        printf("* Last block in this bundle.\n");
-    }
-    if (flags & BPV6_BLOCKFLAG_DISCARD_BLOCK_FAILURE) {
-        printf("* Block should be discarded upon failure to process.\n");
-    }
-    if (flags & BPV6_BLOCKFLAG_DISCARD_BUNDLE_FAILURE) {
-        printf("* Bundle should be discarded upon failure to process.\n");
-    }
-    if (flags & BPV6_BLOCKFLAG_EID_REFERENCE) {
-        printf("* This block references elements from the dictionary.\n");
-    }
-    if (flags & BPV6_BLOCKFLAG_FORWARD_NOPROCESS) {
-        printf("* This block was forwarded without being processed.\n");
-    }
-
-}
 
 bool Bpv6CbhePrimaryBlock::HasCustodyFlagSet() const {
     return ((m_bundleProcessingControlFlags & BPV6_BUNDLEFLAG::CUSTODY_REQUESTED) != BPV6_BUNDLEFLAG::NO_FLAGS_SET);
