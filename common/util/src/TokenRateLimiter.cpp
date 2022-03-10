3ad0a9c1ffe9e8ad18ba05eba7d4ae7ee3e5ea53
Brian Tomko <brian.j.tomko@nasa.gov>
2022-03-10 16:09:38 -0500

    Add the TokenRateLimiter class and unit tests which were contributed by Brian Sipos.

---
+++ common/util/src/TokenRateLimiter.cpp
@@ -0,0 +1,42 @@
+#include "TokenRateLimiter.h"
+#include <iostream>
+
+TokenRateLimiter::TokenRateLimiter() : m_rateTokens(0), m_limit(0), m_remain(0) {}
+
+TokenRateLimiter::~TokenRateLimiter() {}
+
+void TokenRateLimiter::SetRate(const uint64_t tokens, const boost::posix_time::time_duration & interval, const boost::posix_time::time_duration & window) {
+    if (interval.is_special()) {
+        return;
+
+    }
+    m_rateTokens = tokens;
+    m_rateInterval = interval;
+
+    m_limit = m_rateTokens * window.ticks();
+    m_remain = m_limit;
+}
+
+void TokenRateLimiter::AddTime(const boost::posix_time::time_duration & interval) {
+    if (interval.is_special()) {
+        return;
+    }
+    const uint64_t delta = m_rateTokens * interval.ticks();
+    m_remain += delta;
+    if (m_remain > m_limit) {
+        m_remain = m_limit;
+    }
+}
+
+uint64_t TokenRateLimiter::GetRemainingTokens() const {
+    return m_remain / m_rateInterval.ticks();
+}
+
+bool TokenRateLimiter::TakeTokens(const uint64_t tokens) {
+    const uint64_t delta = tokens * m_rateInterval.ticks();
+    if (delta > m_remain) {
+        return false;
+    }
+    m_remain -= delta;
+    return true;
+}
---
+++ common/util/test/TestTokenRateLimiter.cpp
@@ -0,0 +1,67 @@
+#include <boost/test/unit_test.hpp>
+#include "TokenRateLimiter.h"
+
+BOOST_AUTO_TEST_CASE(TokenRateLimiterLowRate)
+{
+    TokenRateLimiter limiter;
+    limiter.SetRate(
+        50, // 20ms per token
+        boost::posix_time::seconds(1),
+        boost::posix_time::milliseconds(100)
+    );
+
+    // deplete the tokens
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), 5);
+    BOOST_REQUIRE(limiter.TakeTokens(3));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), 2);
+    BOOST_REQUIRE(!limiter.TakeTokens(3));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), 2);
+    BOOST_REQUIRE(limiter.TakeTokens(2));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), 0);
+
+    // add fractional times until fully available
+    limiter.AddTime(boost::posix_time::milliseconds(10));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), 0);
+    limiter.AddTime(boost::posix_time::milliseconds(8));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), 0);
+    limiter.AddTime(boost::posix_time::milliseconds(2));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), 1);
+    // next unit time
+    limiter.AddTime(boost::posix_time::milliseconds(20));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), 2);
+
+    // burst limit
+    limiter.AddTime(boost::posix_time::seconds(2));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), 5);
+}
+
+BOOST_AUTO_TEST_CASE(TokenRateLimiterHighRate)
+{
+    static constexpr uint64_t u64_1e9 = 1000000000;
+    static constexpr uint64_t u64_1e8 = 100000000;
+    static constexpr uint64_t u64_5e7 = 50000000;
+    static constexpr uint64_t u64_3e7 = 30000000;
+    static constexpr uint64_t u64_2e7 = 20000000;
+    TokenRateLimiter limiter;
+    limiter.SetRate(
+        u64_1e9, // 1ns per token
+        boost::posix_time::seconds(1),
+        boost::posix_time::milliseconds(100)
+    );
+
+    // deplete the tokens
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), u64_1e8);
+    BOOST_REQUIRE(limiter.TakeTokens(u64_5e7));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), u64_5e7);
+    BOOST_REQUIRE(limiter.TakeTokens(u64_3e7));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), u64_2e7);
+    BOOST_REQUIRE(limiter.TakeTokens(u64_2e7));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), 0);
+
+    limiter.AddTime(boost::posix_time::microseconds(1));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), 1000);
+
+    // burst limit
+    limiter.AddTime(boost::posix_time::seconds(2));
+    BOOST_REQUIRE_EQUAL(limiter.GetRemainingTokens(), u64_1e8);
+}
