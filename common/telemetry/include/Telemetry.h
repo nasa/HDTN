137a2249183ed54d2bcc8de3900dfdb05045634b
blakelafuente <blake.a.lafuente@nasa.gov>
2022-05-24 15:18:42 -0400

    add endian code

--- common/telemetry/include/Telemetry.h
+++ common/telemetry/include/Telemetry.h
@@ -2,7 +2,7 @@
  * @file Telemetry.h
  * @author  Blake LaFuente
  *
- * @copyright Copyright © 2021 United States Government as represented by
+ * @copyright Copyright ï¿½ 2021 United States Government as represented by
  * the National Aeronautics and Space Administration.
  * No copyright is claimed in the United States under Title 17, U.S.Code.
  * All Other Rights Reserved.
@@ -22,8 +22,30 @@
 #include <cstdint>
 #include "telemetry_definitions_export.h"
 
-struct IngressTelemetry_t {
-    uint64_t data1;
+struct IngressTelemetry_t{
+    double bundleDataRate;
+    double averageDataRate;
+    double totalData;
+    uint64_t bundleCountEgress;
+    uint64_t bundleCountStorage;
+
+    TELEMETRY_DEFINITIONS_EXPORT void ToLittleEndianInplace();
+    TELEMETRY_DEFINITIONS_EXPORT void ToNativeEndianInplace();
+};
+
+struct EgressTelemetry_t{
+    uint64_t egressBundleCount;
+    double egressBundleData;
+    uint64_t egressMessageCount;
+
+    TELEMETRY_DEFINITIONS_EXPORT void ToLittleEndianInplace();
+    TELEMETRY_DEFINITIONS_EXPORT void ToNativeEndianInplace();
+
+};
+
+struct StorageTelemetry_t{
+    uint64_t totalBundlesErasedFromStorage;
+    uint64_t totalBundlesSentToEgressFromStorage;
 
     TELEMETRY_DEFINITIONS_EXPORT void ToLittleEndianInplace();
     TELEMETRY_DEFINITIONS_EXPORT void ToNativeEndianInplace();
--- common/telemetry/src/Telemetry.cpp
+++ common/telemetry/src/Telemetry.cpp
@@ -2,7 +2,7 @@
  * @file Telemetry.cpp
  * @author  Blake LaFuente
  *
- * @copyright Copyright © 2021 United States Government as represented by
+ * @copyright Copyright ï¿½ 2021 United States Government as represented by
  * the National Aeronautics and Space Administration.
  * No copyright is claimed in the United States under Title 17, U.S.Code.
  * All Other Rights Reserved.
@@ -16,8 +16,39 @@
 #include <boost/endian/conversion.hpp>
 
 void IngressTelemetry_t::ToLittleEndianInplace() {
-    boost::endian::native_to_little_inplace(data1);
+    //boost::endian::native_to_little_inplace(averageDataRate);
+    //boost::endian::native_to_little_inplace(bundleDataRate);
+    //boost::endian::native_to_little_inplace(totalData);
+    boost::endian::native_to_little_inplace(bundleCountEgress);
+    boost::endian::native_to_little_inplace(bundleCountStorage);
 }
+
 void IngressTelemetry_t::ToNativeEndianInplace() {
-    boost::endian::little_to_native_inplace(data1);
+    //boost::endian::little_to_native_inplace(bundleDataRate);
+    //boost::endian::little_to_native_inplace(averageDataRate);
+    //boost::endian::little_to_native_inplace(totalData);
+    boost::endian::little_to_native_inplace(bundleCountEgress);
+    boost::endian::little_to_native_inplace(bundleCountStorage);
+}
+
+void EgressTelemetry_t::ToLittleEndianInplace(){
+    boost::endian::native_to_little_inplace(egressBundleCount);
+    //boost::endian::native_to_little_inplace(egressBundleData);
+    boost::endian::native_to_little_inplace(egressMessageCount);
+}
+
+void EgressTelemetry_t::ToNativeEndianInplace(){
+    boost::endian::little_to_native_inplace(egressBundleCount);
+    //boost::endian::little_to_native_inplace(egressBundleData);
+    boost::endian::little_to_native_inplace(egressMessageCount);
+}
+
+void StorageTelemetry_t::ToLittleEndianInplace(){
+    boost::endian::native_to_little_inplace(totalBundlesErasedFromStorage);
+    boost::endian::native_to_little_inplace(totalBundlesSentToEgressFromStorage);
+}
+
+void StorageTelemetry_t::ToNativeEndianInplace(){
+    boost::endian::little_to_native_inplace(totalBundlesErasedFromStorage);
+    boost::endian::little_to_native_inplace(totalBundlesSentToEgressFromStorage);
 }
