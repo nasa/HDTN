687cba12b11bab195b628aba958fd68d65c3e955
trecker <timothy.recker@nasa.gov>
2022-07-05 17:40:51 -0400

    make test output more info

--- common/cgr/test/TestDijkstra.cpp
+++ common/cgr/test/TestDijkstra.cpp
@@ -5,17 +5,21 @@
 
 BOOST_AUTO_TEST_CASE(DijkstraSimpleTestCase)
 {
+	std::cout << "Reading contact plan..." << std::endl;
 	const boost::filesystem::path contactRootDir = Environment::GetPathHdtnSourceRoot() / "common" / "cgr" / "test";
 	const std::string contactFile = (contactRootDir / "contactPlan_RoutingTest.json").string();
+
 	std::vector<cgr::Contact> contactPlan = cgr::cp_load(contactFile);
-	//BOOST_REQUIRE(contactPlan);
-	BOOST_CHECK(contactPlan.size() == 8);
+	int numContacts = contactPlan.size();
+	std::cout << "Contact plan with " << numContacts << " contacts read" << std::endl;
+	BOOST_CHECK(numContacts == 8);
 
 	cgr::Contact rootContact = cgr::Contact(1, 1, 0, cgr::MAX_SIZE, 100, 1.0, 0);
+	BOOST_REQUIRE(&rootContact);
 	rootContact.arrival_time = 0;
 
 	cgr::Route bestRoute = cgr::dijkstra(&rootContact, 4, contactPlan);
-	//BOOST_REQUIRE(bestRoute);
+	BOOST_REQUIRE(&bestRoute);
 	int nextHop = bestRoute.next_node;
 	std::cout << "Next hop is: " << std::to_string(nextHop) << std::endl;
 	BOOST_CHECK(nextHop == 2);
