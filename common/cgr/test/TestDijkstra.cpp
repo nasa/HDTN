1b9c23e2617ecf711971d91e6fc0c5c98334f326
trecker <timothy.recker@nasa.gov>
2022-07-05 17:19:26 -0400

    accurately replicate integrated_tests/cgr_tests, using libcgr instead of CgrServer; passes.

--- common/cgr/test/TestDijkstra.cpp
+++ common/cgr/test/TestDijkstra.cpp
@@ -6,12 +6,17 @@
 BOOST_AUTO_TEST_CASE(DijkstraSimpleTestCase)
 {
 	const boost::filesystem::path contactRootDir = Environment::GetPathHdtnSourceRoot() / "common" / "cgr" / "test";
-	const std::string contactFile = (contactRootDir / "contactPlan.json").string();
+	const std::string contactFile = (contactRootDir / "contactPlan_RoutingTest.json").string();
 	std::vector<cgr::Contact> contactPlan = cgr::cp_load(contactFile);
 	//BOOST_REQUIRE(contactPlan);
+	BOOST_CHECK(contactPlan.size() == 8);
 
 	cgr::Contact rootContact = cgr::Contact(1, 1, 0, cgr::MAX_SIZE, 100, 1.0, 0);
+	rootContact.arrival_time = 0;
+
 	cgr::Route bestRoute = cgr::dijkstra(&rootContact, 4, contactPlan);
 	//BOOST_REQUIRE(bestRoute);
-	BOOST_CHECK(bestRoute.next_node == 2);
+	int nextHop = bestRoute.next_node;
+	std::cout << "Next hop is: " << std::to_string(nextHop) << std::endl;
+	BOOST_CHECK(nextHop == 2);
 }
