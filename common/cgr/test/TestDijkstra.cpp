939501b28dbad0318b53622049b16c55f8766ab7
trecker <timothy.recker@nasa.gov>
2022-07-06 12:04:43 -0400

    test pyCGR's example scenario and contact plan

--- common/cgr/test/TestDijkstra.cpp
+++ common/cgr/test/TestDijkstra.cpp
@@ -3,7 +3,7 @@
 #include "Environment.h"
 #include <boost/algorithm/string.hpp>
 
-BOOST_AUTO_TEST_CASE(DijkstraSimpleTestCase)
+BOOST_AUTO_TEST_CASE(DijkstraRoutingTestCase)
 {
 	std::cout << "Reading contact plan..." << std::endl;
 	const boost::filesystem::path contactRootDir = Environment::GetPathHdtnSourceRoot() / "common" / "cgr" / "test";
@@ -24,4 +24,27 @@ BOOST_AUTO_TEST_CASE(DijkstraSimpleTestCase)
 	int nextHop = bestRoute.next_node;
 	std::cout << "Next hop is: " << std::to_string(nextHop) << std::endl;
 	BOOST_CHECK(nextHop == 2);
+}
+
+BOOST_AUTO_TEST_CASE(DijkstraPyCGRTutorialTestCase)
+{
+	std::cout << "Reading contact plan..." << std::endl;
+	const boost::filesystem::path contactRootDir = Environment::GetPathHdtnSourceRoot() / "common" / "cgr" / "test";
+	const std::string contactFile = (contactRootDir / "cgrTutorial.json").string();
+
+	std::vector<cgr::Contact> contactPlan = cgr::cp_load(contactFile);
+	int numContacts = contactPlan.size();
+	std::cout << "Contact plan with " << numContacts << " contacts read" << std::endl;
+	BOOST_CHECK(numContacts == 16);
+
+	cgr::Contact rootContact = cgr::Contact(1, 1, 0, cgr::MAX_SIZE, 100, 1.0, 0);
+	BOOST_REQUIRE(&rootContact);
+	rootContact.arrival_time = 0;
+
+	std::cout << "Finding best path using dijkstra's..." << std::endl;
+	cgr::Route bestRoute = cgr::dijkstra(&rootContact, 5, contactPlan);
+	BOOST_REQUIRE(&bestRoute);
+	int nextHop = bestRoute.next_node;
+	std::cout << "Next hop is: " << std::to_string(nextHop) << std::endl;
+	BOOST_CHECK(nextHop == 3);
 }
--- test_scripts_windows/test_libcgr.bat
+++ test_scripts_windows/test_libcgr.bat
@@ -1,3 +1,3 @@
 @Echo off
 
-START "Unit tests" /D "%HDTN_BUILD_ROOT%" "cmd /k" "%HDTN_BUILD_ROOT%\tests\unit_tests\unit-tests.exe" "--run_test=DijkstraSimpleTestCase"
+START "Unit tests" /D "%HDTN_BUILD_ROOT%" "cmd /k" "%HDTN_BUILD_ROOT%\tests\unit_tests\unit-tests.exe" "--run_test=DijkstraPyCGRTutorialTestCase,DijkstraRoutingTestCase"
