320dee5df6830e6bc527ba1baab05dc78ac5057b
trecker <timothy.recker@nasa.gov>
2022-07-08 10:52:27 -0400

    examine dijkstra route's more thoroughly

--- common/cgr/test/TestDijkstra.cpp
+++ common/cgr/test/TestDijkstra.cpp
@@ -5,6 +5,7 @@
 
 BOOST_AUTO_TEST_CASE(DijkstraRoutingTestCase)
 {
+	// Route from node 1 to 4 using the "RoutingTest" contact plan
 	std::cout << "Reading contact plan..." << std::endl;
 	const boost::filesystem::path contactRootDir = Environment::GetPathHdtnSourceRoot() / "common" / "cgr" / "test";
 	const std::string contactFile = (contactRootDir / "contactPlan_RoutingTest.json").string();
@@ -15,19 +16,30 @@ BOOST_AUTO_TEST_CASE(DijkstraRoutingTestCase)
 	BOOST_CHECK(numContacts == 8);
 
 	cgr::Contact rootContact = cgr::Contact(1, 1, 0, cgr::MAX_SIZE, 100, 1.0, 0);
-	BOOST_REQUIRE(&rootContact);
 	rootContact.arrival_time = 0;
 
 	std::cout << "Finding best path using dijkstra's..." << std::endl;
 	cgr::Route bestRoute = cgr::dijkstra(&rootContact, 4, contactPlan);
-	BOOST_REQUIRE(&bestRoute);
+
+	// Todo: need to finalize what a failed dijkstra search should return
+	//BOOST_REQUIRE(&bestRoute);
 	int nextHop = bestRoute.next_node;
-	std::cout << "Next hop is: " << std::to_string(nextHop) << std::endl;
 	BOOST_CHECK(nextHop == 2);
+
+	std::cout << "Route found (next hop is " << nextHop << "):" << std::endl << bestRoute << std::endl;
+
+	int expectedHops = 2;
+	std::vector<cgr::Contact> expectedContacts({ contactPlan[1], contactPlan[2]});
+	std::vector<cgr::Contact> hops = bestRoute.get_hops();
+	BOOST_CHECK(hops.size() == expectedHops);
+	for (int i = 0; i < expectedHops; ++i) {
+		BOOST_CHECK(hops[i] == expectedContacts[i]);
+	};
 }
 
 BOOST_AUTO_TEST_CASE(DijkstraPyCGRTutorialTestCase)
 {
+	// Route from node 1 to node 5 using the contact plan from the pyCGR tutorial
 	std::cout << "Reading contact plan..." << std::endl;
 	const boost::filesystem::path contactRootDir = Environment::GetPathHdtnSourceRoot() / "common" / "cgr" / "test";
 	const std::string contactFile = (contactRootDir / "cgrTutorial.json").string();
@@ -38,13 +50,22 @@ BOOST_AUTO_TEST_CASE(DijkstraPyCGRTutorialTestCase)
 	BOOST_CHECK(numContacts == 16);
 
 	cgr::Contact rootContact = cgr::Contact(1, 1, 0, cgr::MAX_SIZE, 100, 1.0, 0);
-	BOOST_REQUIRE(&rootContact);
 	rootContact.arrival_time = 0;
 
 	std::cout << "Finding best path using dijkstra's..." << std::endl;
 	cgr::Route bestRoute = cgr::dijkstra(&rootContact, 5, contactPlan);
-	BOOST_REQUIRE(&bestRoute);
+	
+	//BOOST_REQUIRE(&bestRoute);
 	int nextHop = bestRoute.next_node;
-	std::cout << "Next hop is: " << std::to_string(nextHop) << std::endl;
 	BOOST_CHECK(nextHop == 3);
+
+	std::cout << "Route found (next hop is " << nextHop << "):" << std::endl << bestRoute << std::endl;
+
+	int expectedHops = 3;
+	std::vector<cgr::Contact> expectedContacts({ contactPlan[4], contactPlan[6], contactPlan[10]});
+	std::vector<cgr::Contact> hops = bestRoute.get_hops();
+	BOOST_CHECK(hops.size() == expectedHops);
+	for (int i = 0; i < expectedHops; ++i) {
+		BOOST_CHECK(hops[i] == expectedContacts[i]);
+	};
 }
