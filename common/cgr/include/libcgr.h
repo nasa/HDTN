b4e4a76713e25b0d33b0e9c581b8d4b8335c1792
trecker <timothy.recker@nasa.gov>
2022-07-08 17:35:46 -0400

    add exports to libcgr header to support windows shared library

--- common/cgr/include/libcgr.h
+++ common/cgr/include/libcgr.h
@@ -5,6 +5,7 @@
 #include <map>
 #include <ostream>
 #include <limits>
+#include "cgr_lib_export.h"
 
 namespace cgr {
 
@@ -32,13 +33,13 @@ public:
     std::vector<Contact> suppressed_next_hop;
     // Forwarding working area
     int first_byte_tx_time, last_byte_tx_time, last_byte_arr_time, effective_volume_limit;
-    void clear_dijkstra_working_area();
-    Contact(nodeId_t frm, nodeId_t to, int start, int end, int rate, float confidence=1, int owlt=1);
-    Contact();
-    ~Contact();
-    bool operator==(const Contact) const;
-    bool operator!=(const Contact) const;
-    friend std::ostream& operator<<(std::ostream&, const Contact&);
+    CGR_LIB_EXPORT void clear_dijkstra_working_area();
+    CGR_LIB_EXPORT Contact(nodeId_t frm, nodeId_t to, int start, int end, int rate, float confidence=1, int owlt=1);
+    CGR_LIB_EXPORT Contact();
+    CGR_LIB_EXPORT ~Contact();
+    CGR_LIB_EXPORT bool operator==(const Contact) const;
+    CGR_LIB_EXPORT bool operator!=(const Contact) const;
+    CGR_LIB_EXPORT friend std::ostream& operator<<(std::ostream&, const Contact&);
 };
 
 
@@ -47,39 +48,38 @@ public:
     nodeId_t to_node, next_node;
     int from_time, to_time, best_delivery_time, volume;
     float confidence;
-    Route(Contact, Route *parent=NULL);
-    Route();
-    ~Route();
+    CGR_LIB_EXPORT Route(Contact, Route *parent=NULL);
+    CGR_LIB_EXPORT Route();
+    CGR_LIB_EXPORT ~Route();
 private:
     Route *parent;
     std::vector<Contact> hops;
     std::map<nodeId_t, bool> __visited;
 public:
-    Contact get_last_contact();
-    bool visited(nodeId_t node);
-    void append(Contact contact);
-    void refresh_metrics();
-    bool eligible(Contact contact);
-    std::vector<Contact> get_hops();
-    friend std::ostream& operator<<(std::ostream&, const Route&);
+    CGR_LIB_EXPORT Contact get_last_contact();
+    CGR_LIB_EXPORT bool visited(nodeId_t node);
+    CGR_LIB_EXPORT void append(Contact contact);
+    CGR_LIB_EXPORT void refresh_metrics();
+    CGR_LIB_EXPORT bool eligible(Contact contact);
+    CGR_LIB_EXPORT std::vector<Contact> get_hops();
 };
 
-std::vector<Contact> cp_load(std::string filename, int max_contacts=MAX_SIZE);
+CGR_LIB_EXPORT std::vector<Contact> cp_load(std::string filename, int max_contacts=MAX_SIZE);
 
-Route dijkstra(Contact *root_contact, nodeId_t destination, std::vector<Contact> contact_plan);
+CGR_LIB_EXPORT Route dijkstra(Contact *root_contact, nodeId_t destination, std::vector<Contact> contact_plan);
 
-std::vector<Route> yen(nodeId_t source, nodeId_t destination, int currTime, std::vector<Contact> contactPlan, int numRoutes);
+CGR_LIB_EXPORT std::vector<Route> yen(nodeId_t source, nodeId_t destination, int currTime, std::vector<Contact> contactPlan, int numRoutes);
 
 template <typename T>
-bool vector_contains(std::vector<T> vec, T ele);
+CGR_LIB_EXPORT bool vector_contains(std::vector<T> vec, T ele);
 
 class EmptyContainerError: public std::exception {
-    virtual const char* what() const throw();
+    CGR_LIB_EXPORT virtual const char* what() const throw();
 };
 
-std::ostream& operator<<(std::ostream &out, const std::vector<Contact> &obj);
-std::ostream& operator<<(std::ostream &out, const Contact &obj);
-std::ostream& operator<<(std::ostream &out, const Route &obj);
+CGR_LIB_EXPORT std::ostream& operator<<(std::ostream &out, const std::vector<Contact> &obj);
+CGR_LIB_EXPORT std::ostream& operator<<(std::ostream &out, const Contact &obj);
+CGR_LIB_EXPORT std::ostream& operator<<(std::ostream &out, const Route &obj);
 
 } // namespace cgr
 
