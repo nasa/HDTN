cb7161b5039a7e341ee46d57df303167f53eedd3
Ethan Schweinsberg <ethan.e.schweinsberg@nasa.gov>
2022-11-04 13:14:43 -0400

    more spacing fixes

--- module/scheduler/src/scheduler.cpp
+++ module/scheduler/src/scheduler.cpp
@@ -350,9 +350,9 @@ void Scheduler::EgressEventsHandler() {
 
             if (event == 1) {
                 if (m_mapContactUp[contact]) {
-	            LOG_INFO(subprocess) << "EgressEventsHandler Sending Link Up event ";
-                SendLinkUp(srcNode, destNode, finalDestEid.nodeId);
-		}
+                    LOG_INFO(subprocess) << "EgressEventsHandler Sending Link Up event ";
+                    SendLinkUp(srcNode, destNode, finalDestEid.nodeId);
+                }
             } else {
                 LOG_INFO(subprocess) << "EgressEventsHandler Sending Link Down event ";
                 SendLinkDown(srcNode, destNode, finalDestEid.nodeId);
@@ -379,7 +379,7 @@ void Scheduler::UisEventsHandler() {
         }
         std::shared_ptr<boost::property_tree::ptree> ptPtr = std::make_shared<boost::property_tree::ptree>(JsonSerializable::GetPropertyTreeFromCharArray((char*)message.data(), message.size()));
         boost::asio::post(m_ioService, boost::bind(&Scheduler::ProcessContactsPtPtr, this, std::move(ptPtr), hdr.using_unix_timestamp));
-       LOG_INFO(subprocess) << "received Reload contact Plan event with data " << (char*)message.data();
+        LOG_INFO(subprocess) << "received Reload contact Plan event with data " << (char*)message.data();
     }
     else {
         LOG_ERROR(subprocess) << "error in Scheduler::UisEventsHandler: unknown hdr " << hdr.base.type;
