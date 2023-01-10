5b70a8fd2f0b3db7b1a3a5d8b583dc92e02baa66
tphnicholas <tphnicholas@gmail.com>
2023-01-10 16:09:02 +0200

    Add docs for LtpClientServiceDataToSend

--- common/util/include/LtpClientServiceDataToSend.h
+++ common/util/include/LtpClientServiceDataToSend.h
@@ -36,41 +36,134 @@
 #include "hdtn_util_export.h"
 #include <boost/core/noncopyable.hpp>
 
+/**
+ * Data States (union-like state):
+ * 1. Byte buffer -> m_vector is active.
+ * 2. ZMQ message -> m_zmqMessage is active.
+ */
 class LtpClientServiceDataToSend : private boost::noncopyable {
 public:
+    /**
+     * Initialize an empty packet buffer
+     * @post Active data state: Byte buffer
+     */
     HDTN_UTIL_EXPORT LtpClientServiceDataToSend(); //a default constructor: X()
+    
+    /**
+     * Initialize the packet buffer to the given byte buffer.
+     * @param vec The byte buffer packet to set.
+     * @post Active data state: Byte buffer.
+     * @post The argument to vec is left in a moved-from state.
+     */
     HDTN_UTIL_EXPORT LtpClientServiceDataToSend(std::vector<uint8_t> && vec);
+    
+    /**
+     * If previously holding a ZMQ message, clean up its resources.
+     * Initialize the packet buffer to the given byte buffer.
+     * @param vec The byte buffer packet to set.
+     * @return *this.
+     * @post Active data state: Byte buffer.
+     * @post The argument to vec is left in a moved-from state.
+     */
     HDTN_UTIL_EXPORT LtpClientServiceDataToSend& operator=(std::vector<uint8_t> && vec); //a move assignment: operator=(X&&)
 #ifdef LTP_CLIENT_SERVICE_DATA_TO_SEND_SUPPORT_ZMQ
+    /**
+     * Initialize the packet buffer to the given ZMQ message.
+     * @param zmqMessage The ZMQ message to set.
+     * @post Active data state: ZMQ message.
+     */
     HDTN_UTIL_EXPORT LtpClientServiceDataToSend(zmq::message_t && zmqMessage);
+    
+    /**
+     * Initialize the packet buffer to the given ZMQ message.
+     * @param zmqMessage The ZMQ message to set.
+     * @return *this.
+     * @post Active data state: ZMQ message.
+     * @post The argument to zmqMessage is left in a moved-from state.
+     */
     HDTN_UTIL_EXPORT LtpClientServiceDataToSend& operator=(zmq::message_t && zmqMessage); //a move assignment: operator=(X&&)
 #endif
+    /// Default destructor
     HDTN_UTIL_EXPORT ~LtpClientServiceDataToSend(); //a destructor: ~X()
+    
+    /// Deleted copy constructor
     HDTN_UTIL_EXPORT LtpClientServiceDataToSend(const LtpClientServiceDataToSend& o) = delete; //disallow copying
+    
+    /// Default move constructor
     HDTN_UTIL_EXPORT LtpClientServiceDataToSend(LtpClientServiceDataToSend&& o) noexcept; //a move constructor: X(X&&)
+    
+    /// Deleted copy assignment operator
     HDTN_UTIL_EXPORT LtpClientServiceDataToSend& operator=(const LtpClientServiceDataToSend& o) = delete; //disallow copying
+    
+    /// Default move assignment operator
     HDTN_UTIL_EXPORT LtpClientServiceDataToSend& operator=(LtpClientServiceDataToSend&& o) noexcept; //a move assignment: operator=(X&&)
+    
+    /**
+     * @param vec The byte buffer to compare.
+     * @return Stored byte buffer == vec.
+     */
     HDTN_UTIL_EXPORT bool operator==(const std::vector<uint8_t> & vec) const; //operator ==
+    
+    /**
+     * @param vec The byte buffer to compare.
+     * @return Stored byte buffer != vec.
+     */
     HDTN_UTIL_EXPORT bool operator!=(const std::vector<uint8_t> & vec) const; //operator !=
+    
+    /** Get the begin pointer of the primary data buffer.
+     *
+     * Valid regardless of active data state.
+     * @return The begin pointer of the primary data buffer.
+     */
     HDTN_UTIL_EXPORT uint8_t * data() const;
+    
+    /** Get the size the primary data buffer.
+     *
+     * Valid regardless of active data state.
+     * @return The size of the primary data buffer.
+     */
     HDTN_UTIL_EXPORT std::size_t size() const;
+    
+    /** Clean up internal data buffers.
+     *
+     * Valid regardless of active data state.
+     * @param setSizeValueToZero Whether to reset the tracked size of the primary data buffer.
+     * @post The primary data buffer begin pointer is set to NULL.
+     */
     HDTN_UTIL_EXPORT void clear(bool setSizeValueToZero);
+    
+    /** Get the stored byte buffer.
+     *
+     * Reference to a default-constructed object when not the active data state.
+     * @return The stored byte buffer.
+     */
     HDTN_UTIL_EXPORT std::vector<uint8_t> & GetVecRef();
+    
+    /** Get the stored ZMQ message.
+     *
+     * Reference to a default-constructed object when not the active data state.
+     * @return The stored ZMQ message.
+     */
     HDTN_UTIL_EXPORT zmq::message_t & GetZmqRef();
 
 public:
+    /// Attached user data
     std::vector<uint8_t> m_userData;
 
 private:
+    /// Stored byte buffer
     std::vector<uint8_t> m_vector;
 #ifdef LTP_CLIENT_SERVICE_DATA_TO_SEND_SUPPORT_ZMQ
+    /// Stored ZMQ message
     zmq::message_t m_zmqMessage;
 #endif
+    /// Primary data buffer begin pointer
     uint8_t * m_ptrData;
+    /// Primary data buffer size
     std::size_t m_size;
 };
 
-struct UdpSendPacketInfo : private boost::noncopyable {
+struct UdpSendPacketInfo : private boost::noncopyable { //TODO: Docs, complete
     std::vector<boost::asio::const_buffer> constBufferVec;
     std::shared_ptr<std::vector<std::vector<uint8_t> > > underlyingDataToDeleteOnSentCallback;
     std::shared_ptr<LtpClientServiceDataToSend> underlyingCsDataToDeleteOnSentCallback;
