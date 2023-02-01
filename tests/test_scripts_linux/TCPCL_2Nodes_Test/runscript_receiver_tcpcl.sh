dad3474098e5d7a65b5e07e76813ae940b5ceb5f
Blake LaFuente <blake.a.lafuente@nasa.gov>
2023-02-01 16:09:10 -0500

    Add files for STCP 2 Node Test

---
+++ tests/test_scripts_linux/TCPCL_2Nodes_Test/runscript_receiver_tcpcl.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# path variables
+config_files=$HDTN_SOURCE_ROOT/config_files
+hdtn_config=$config_files/hdtn/hdtn_Node2_tcpcl.json
+sink_config=$config_files/inducts/bpsink_one_tcpcl_port4558_2NodesTest.json
+
+cd $HDTN_SOURCE_ROOT
+
+# bpsink
+./build/common/bpcodec/apps/bpsink-async --my-uri-eid=ipn:2.1 --inducts-config-file=$sink_config &
+sleep 3
+
+# HDTN one process
+./build/module/hdtn_one_process/hdtn-one-process --contact-plan-file=contactPlan2Nodes.json  --hdtn-config-file=$hdtn_config &
+sleep 10
+
---
+++ tests/test_scripts_linux/TCPCL_2Nodes_Test/runscript_sender_tcpcl.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# path variables
+config_files=$HDTN_SOURCE_ROOT/config_files
+hdtn_config=$config_files/hdtn/hdtn_Node1_tcpcl.json
+gen_config=$config_files/outducts/bpgen_one_tcpcl_port4556_2NodesTest.json
+
+cd $HDTN_SOURCE_ROOT
+
+# HDTN one process
+./build/module/hdtn_one_process/hdtn-one-process  --contact-plan-file=contactPlan2Nodes.json --hdtn-config-file=$hdtn_config &
+sleep 10
+
+#bpgen
+./build/common/bpcodec/apps/bpgen-async --bundle-size=100000 --bundle-rate=0 --my-uri-eid=ipn:1.1 --dest-uri-eid=ipn:2.1 --duration=40 --outducts-config-file=$gen_config &
+sleep 8
