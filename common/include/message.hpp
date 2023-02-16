0ee1161a06eec9139f4be97265532abc85a2e732
Ethan Schweinsberg <ethan.e.schweinsberg@nasa.gov>
2023-02-16 10:16:27 -0500

    add clarifying comments to new fields

--- common/include/message.hpp
+++ common/include/message.hpp
@@ -159,10 +159,10 @@ struct IreleaseChangeHdr {
     uint8_t unused1;
     uint8_t unused2;
     uint8_t unused3;
-    uint8_t isPhysical;
+    uint8_t isPhysical; // whether the link status change is due to physical (link disruption) or a contact change
     uint64_t outductArrayIndex; //outductUuid
     uint64_t rateBps; // (start events only)
-    uint64_t duration;  // sec (start events only)
+    uint64_t duration;  // the duration of the contact (s) (link up events only)
     uint64_t prevHopNodeId;
     uint64_t nextHopNodeId;
     uint64_t time;
