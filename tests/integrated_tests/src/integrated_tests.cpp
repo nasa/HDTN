3514a702c381a8a08721181422251576430054c4
Alyssa <alyssa.m.brewer@nasa.gov>
2023-03-27 15:32:30 -0400

    Adding file transfer test

--- tests/integrated_tests/src/integrated_tests.cpp
+++ tests/integrated_tests/src/integrated_tests.cpp
@@ -505,7 +505,101 @@ bool TestHDTNStorageModeLTPv7() {
     return true;
 }
 
+bool TestHDTNFileTransferLTP() {
 
+    Delay(DELAY_TEST);
+
+    bool runningBpgen = true;
+    bool runningBpsink = true;
+    bool runningHdtnOneProcess = true; 
+
+    uint64_t bundlesSentBpgen[1] = {0};
+    OutductFinalStats finalStats[1];
+    FinalStatsBpSink finalStatsBpSink[1];
+    uint64_t bundlesReceivedBpsink[1]= {0};
+    uint64_t bundleCountStorage = 0;
+    uint64_t bundleCountEgress = 0;
+    uint64_t bundleCountIngress = 0;
+
+    // Start threads
+    Delay(DELAY_THREAD);
+
+    //bpsink
+    static const std::string bpsinkConfigArg = "--inducts-config-file=" + (Environment::GetPathHdtnSourceRoot() / "config_files" / "inducts" / "bpsink_one_ltp_port4558.json").string();
+    static const char * argsBpsink[] = { "bpsink",  "--save-directory=received", "--my-uri-eid=ipn:2.1", bpsinkConfigArg.c_str(), NULL };
+    std::thread threadBpsink(RunBpsinkAsync, argsBpsink, 4, std::ref(runningBpsink), &bundlesReceivedBpsink[0],
+        &finalStatsBpSink[0]);
+
+    Delay(DELAY_THREAD);
+
+    //HDTN One Process
+    static const std::string hdtnConfigArg = "--hdtn-config-file=" + (Environment::GetPathHdtnSourceRoot() / "config_files" / "hdtn" / "hdtn_ingress1ltp_port4556_egress1ltp_port4558flowid2.json").string();
+    const boost::filesystem::path contactsFile = "contactPlanCutThroughMode.json";
+    const std::string eventFileArg = "--contact-plan-file=" + contactsFile.string();
+    const char * argsHdtnOneProcess[] = { "HdtnOneProcess", eventFileArg.c_str(), hdtnConfigArg.c_str(), NULL };
+    std::thread threadHdtn(RunHdtnOneProcess, argsHdtnOneProcess, 3, std::ref(runningHdtnOneProcess), &bundleCountStorage,
+		             &bundleCountEgress, &bundleCountIngress);
+
+    Delay(10);
+
+    //Bpgen
+    static const std::string bpgenConfigArg = 
+	"--outducts-config-file=" + (Environment::GetPathHdtnSourceRoot() / "config_files" / "outducts" / "bpgen_one_ltp_port4556_thisengineid200.json").string();
+    static const char * argsBpgen[] = { "bpgen",  "--my-uri-eid=ipn:1.1", "--dest-uri-eid=ipn:2.1", "--max-bundle-size-bytes=4000000", "--file-or-folder-path=test.txt", bpgenConfigArg.c_str(), NULL };
+    std::thread threadBpgen(RunBpgenAsync,argsBpgen, 7, std::ref(runningBpgen), &bundlesSentBpgen[0], &finalStats[0]);
+
+    // Allow time for data to flow
+    boost::this_thread::sleep(boost::posix_time::seconds(8));
+
+    // Stop threads
+    runningBpgen = false;
+    threadBpgen.join();
+
+    runningHdtnOneProcess = false;
+    threadHdtn.join();
+
+    runningBpsink = false;
+    threadBpsink.join();
+
+    // Verify results
+    uint64_t totalBundlesBpgen = 0;
+    for (int i=0; i<1; i++) {
+        totalBundlesBpgen += bundlesSentBpgen[i];
+    }
+ 
+    uint64_t totalBundlesBpsink = 0;
+    for(int i=0; i<1; i++) {
+        totalBundlesBpsink += bundlesReceivedBpsink[i];
+    }
+
+    if (bundleCountIngress != bundleCountEgress) {
+        BOOST_ERROR("Total Bundles received by Ingress (" + std::to_string(bundleCountIngress) + ") != Total bundles received by Egress in Cuthrough Mode "
+                    + std::to_string(bundleCountEgress) + ").");
+        return false;
+    }
+
+    if (totalBundlesBpgen != totalBundlesBpsink) {
+        BOOST_ERROR("Bundles sent by BpGen (" + std::to_string(totalBundlesBpgen) + ") != bundles received by BpSink "
+                    + std::to_string(totalBundlesBpsink) + ").");
+	return false;
+    }
+
+    if (totalBundlesBpgen != bundleCountIngress) {
+        BOOST_ERROR("Bundles sent by BpGen (" + std::to_string(totalBundlesBpgen) + ") !=  bundles received by Ingress "
+                + std::to_string(bundleCountIngress) + ").");
+        return false;
+    }
+
+    if (totalBundlesBpgen != bundleCountEgress) {
+        BOOST_ERROR("Bundles sent by BpGen (" + std::to_string(totalBundlesBpgen) + ") != bundles received by Egress "
+                + std::to_string(bundleCountEgress) + ").");
+        return false;
+    }
+    
+    return true;
+}
+
+/*
 bool TestHDTNCutThroughModeTCPCL() {
 
     Delay(DELAY_TEST);
@@ -535,8 +629,7 @@ bool TestHDTNCutThroughModeTCPCL() {
     Delay(DELAY_THREAD);
 
     //HDTN One Process
-    static const std::string hdtnConfigArg = "--hdtn-config-file=" + (Environment::GetPathHdtnSourceRoot() / "config_files" /
- "hdtn" / "hdtn_ingress1tcpclv4_port4556_egress1tcpclv4_port4558flowid2.json").string();
+    static const std::string hdtnConfigArg = "--hdtn-config-file=" + (Environment::GetPathHdtnSourceRoot() / "config_files" / "hdtn" / "hdtn_ingress1tcpclv4_port4556_egress1tcpclv4_port4558flowid2.json").string();
     const boost::filesystem::path contactsFile = "contactPlanCutThroughMode.json";
     const std::string eventFileArg = "--contact-plan-file=" + contactsFile.string();
     const char * argsHdtnOneProcess[] = { "HdtnOneProcess", eventFileArg.c_str(), hdtnConfigArg.c_str(), NULL };
@@ -894,8 +987,10 @@ bool TestHDTNStorageModeTCPCLv7() {
     return true;
 }
 
+*/
+
 BOOST_GLOBAL_FIXTURE(BoostIntegratedTestsFixture);
-/*
+
 
 BOOST_AUTO_TEST_CASE(it_TestHDTNCutThroughModeLTP, * boost::unit_test::enabled()) {
     std::cout << std::endl << ">>>>>> Running: " << "it_TestHDTNCutThroughModeLTP" << std::endl << std::flush;
@@ -921,8 +1016,14 @@ BOOST_AUTO_TEST_CASE(it_TestHDTNStorageModeLTPv7, * boost::unit_test::enabled())
     BOOST_CHECK(result == true);
 }
 
-*/
+BOOST_AUTO_TEST_CASE(it_TestHDTNFileTransferLTP, * boost::unit_test::enabled()) {
+    std::cout << std::endl << ">>>>>> Running: " << "it_TestHDTNFileTransferLTP" << std::endl << std::flush;
+    bool result = TestHDTNFileTransferLTP();
+    BOOST_CHECK(result == true);
+}
 
+
+/*
 BOOST_AUTO_TEST_CASE(it_TestHDTNCutThroughModeTCPCL, * boost::unit_test::enabled()) {
     std::cout << std::endl << ">>>>>> Running: " << "it_TestHDTNCutThroughModeTCPCL" << std::endl << std::flush;
     bool result = TestHDTNCutThroughModeTCPCL();
