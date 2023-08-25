94b7383933517b7b991b622454926c62da641f82
T-recks <timjrecker@gmail.com>
2023-08-25 09:24:58 -0400

    shorten script run time

--- tests/test_scripts_linux/test_masker_02.sh
+++ tests/test_scripts_linux/test_masker_02.sh
@@ -12,7 +12,7 @@ cd $HDTN_SOURCE_ROOT
 # bpsink1
 ./build/common/bpcodec/apps/bpsink-async --my-uri-eid=ipn:2.1 --inducts-config-file=$sink_config &
 bpsink1_PID=$!
-sleep 3
+sleep 8
 
 # HDTN one process
 ./build/module/hdtn_one_process/hdtn-one-process --hdtn-config-file=$hdtn_config2 --contact-plan-file=masker_test.json &
@@ -20,7 +20,7 @@ oneprocess_PID2=$!
 sleep 10
 
 # cleanup
-sleep 60
+sleep 30
 echo "\nkilling hdtn2..." && kill -2 $oneprocess_PID2
 sleep 2
 echo "\nkilling bpsink1..." && kill -2 $bpsink1_PID
