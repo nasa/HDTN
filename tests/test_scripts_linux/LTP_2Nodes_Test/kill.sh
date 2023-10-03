fa88722ffb0049afa8ee485bce0dd7afa9c17d14
HDTN <hdtn@node1.hdtn.lan>
2023-10-03 18:36:11 +0000

    Added BPSec send script to LTP 2 nodes test

---
+++ tests/test_scripts_linux/LTP_2Nodes_Test/kill.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for j in hdtn-one-process hdtn-ingress hdtn-egress-async hdtn-storage bpsink-async  bpgen-async bpreceivefile bpsendfile hdtn-router telem_cmd_interface udp-delay-sim
+do
+for i in `pidof $j`
+do
+ kill -SIGINT $i
+done
+done
+
+sleep 6
+
+for j in hdtn-one-process hdtn-ingress hdtn-egress-async hdtn-storage bpsink-async bpgen-async bpreceivefile bpsendfile hdtn-router telem_cmd_interface udp-delay-sim
+
+do
+for i in `pidof $j`
+do
+ kill -9 $i
+done
+done
