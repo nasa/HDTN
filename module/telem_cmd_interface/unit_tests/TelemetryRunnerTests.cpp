b84bd19a3e11ad4f22097bb3b830d488e857c612
Shaun M. McKeehan <shaun.mckeehan@nasa.gov>
2024-03-04 14:31:22 -0500

    (#330) fix unit tests when gui_v2 is used

--- module/telem_cmd_interface/unit_tests/TelemetryRunnerTests.cpp
+++ module/telem_cmd_interface/unit_tests/TelemetryRunnerTests.cpp
@@ -9,7 +9,7 @@ BOOST_AUTO_TEST_CASE(TelemetryRunnerInitTestCase)
 {
     TelemetryRunnerProgramOptions options;
     options.m_guiPortNumber = "8086";
-    options.m_guiDocumentRoot = Environment::GetPathHdtnSourceRoot() / "module" / "telem_cmd_interface" / "src" / "gui";
+    options.m_guiDocumentRoot = Environment::GetPathGuiDocumentRoot();
     options.m_hdtnDistributedConfigPtr = std::make_shared<HdtnDistributedConfig>(); //default config, needed since inprocContextPtr is null
     TelemetryRunner runner;
     HdtnConfig hdtnConfig;
