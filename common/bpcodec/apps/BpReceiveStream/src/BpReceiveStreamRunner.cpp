be7b0313c3e4ca981d5ca3d902cbdb8a6ab97394
Brian Tomko <brian.j.tomko@nasa.gov>
2024-05-17 09:49:11 -0400

    (#379) Add copyright header to streaming source files.

--- common/bpcodec/apps/BpReceiveStream/src/BpReceiveStreamRunner.cpp
+++ common/bpcodec/apps/BpReceiveStream/src/BpReceiveStreamRunner.cpp
@@ -1,3 +1,16 @@
+/**
+ * @file BpReceiveStreamRunner.cpp
+ *
+ * @copyright Copyright (c) 2021 United States Government as represented by
+ * the National Aeronautics and Space Administration.
+ * No copyright is claimed in the United States under Title 17, U.S.Code.
+ * All Other Rights Reserved.
+ *
+ * @section LICENSE
+ * Released under the NASA Open Source Agreement (NOSA)
+ * See LICENSE.md in the source root directory for more information.
+ */
+
 #include "BpReceiveStreamRunner.h"
 #include "Logger.h"
 #include "SignalHandler.h"
--- common/streaming/include/AsyncListener.h
+++ common/streaming/include/AsyncListener.h
@@ -1,3 +1,16 @@
+/**
+ * @file AsyncListener.h
+ *
+ * @copyright Copyright (c) 2021 United States Government as represented by
+ * the National Aeronautics and Space Administration.
+ * No copyright is claimed in the United States under Title 17, U.S.Code.
+ * All Other Rights Reserved.
+ *
+ * @section LICENSE
+ * Released under the NASA Open Source Agreement (NOSA)
+ * See LICENSE.md in the source root directory for more information.
+ */
+
 // THIS IS NOT THREAD SAFE 
 #include <boost/asio.hpp>
 #include <boost/process.hpp>
