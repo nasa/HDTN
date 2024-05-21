47d6c232abd3aec5367f2039a3b936d666f885fd
Prash Choksi <prash.choksi@nasa.gov>
2024-05-21 16:28:18 -0400

    Resolve "check streaming test scripts and make sure there is an example ISS can use"

---
+++ tests/test_scripts_linux/Streaming/file_streaming/README.md
@@ -0,0 +1,16 @@
+# file\_streaming
+* Tests the Multimedia file Streaming with HDTN. Check the Audio and Video Streaming section of the README for more details
+ 
+
+## How to use Streaming\_1_Node\_Test
+Run the runscript for the convergence layer needed on the localhost  (LTP, STCP or TCPCL) 
+
+## How to use Streaming\_2_Nodes\_Test
+* Within the "receiver" container, run the receiver runscript 
+* Then wait about 3 seconds
+* When you are done waiting for 3 seconds, within the "sender" container run the sender runscript 
+* Wait about 10 seconds for the sender and receiver programs to initialize and operate
+* View the video stream received as described in the section Viewing Multimedia Streams of the README
+
+
+
---
+++ tests/test_scripts_linux/Streaming/file_streaming/video_with_audio/Streaming_2_Nodes_Test/runscript_sender_va_h264_ltp_streaming.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# HDTN Path Variables
+config_files=$HDTN_SOURCE_ROOT/config_files
+hdtn_config=$config_files/hdtn/hdtn_Node1_ltp.json
+gen_config=$config_files/outducts/bpgen_one_stcp_port4556_2NodesTest.json
+
+
+cd $HDTN_SOURCE_ROOT
+
+
+# HDTN one process
+./build/module/hdtn_one_process/hdtn-one-process --contact-plan-file=$HDTN_SOURCE_ROOT/module/router/contact_plans/contactPlan2Nodes.json --hdtn-config-file=$hdtn_config &
+
+sleep 6
+
+# BpSendStream
+incoming_rtp_port=6565
+./build/common/bpcodec/apps/bpsend_stream  --use-bp-version-7 --bundle-rate=0 --bundle-size=200000  --my-uri-eid=ipn:1.1 --dest-uri-eid=ipn:2.1 --induct-type="udp" --max-incoming-udp-packet-size-bytes=1800 --num-circular-buffer-vectors=3000 --rtp-packets-per-bundle=20 --incoming-rtp-stream-port=$incoming_rtp_port --outducts-config-file=$gen_config &
+
+sleep 3
+
+# GStreamer Pipeline to Start Streaming Video File with Audio
+VIDEO_TO_STREAM_FILEPATH=$HDTN_SOURCE_ROOT/launch.mp4
+gst-launch-1.0 -e filesrc location=$VIDEO_TO_STREAM_FILEPATH \
+    ! qtdemux name=demux \
+    demux.video_0 ! queue ! h264parse ! mpegtsmux name=mux ! rtpmp2tpay ! udpsink host=127.0.0.1 port=$incoming_rtp_port \
+    demux.audio_0 ! queue ! aacparse ! mux. &
+
+sleep infinity
