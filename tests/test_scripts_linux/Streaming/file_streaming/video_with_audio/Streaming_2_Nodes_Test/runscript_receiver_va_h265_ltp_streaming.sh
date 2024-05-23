b6393c459a93e60b8608682f62e569c4dccc04ec
Prash Choksi <prash.choksi@nasa.gov>
2024-05-23 15:34:19 -0400

    (#390) Add File Streaming Runscripts for H.265

---
+++ tests/test_scripts_linux/Streaming/file_streaming/video_with_audio/Streaming_2_Nodes_Test/runscript_receiver_va_h265_ltp_streaming.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# HDTN Path Variables
+config_files=$HDTN_SOURCE_ROOT/config_files
+hdtn_config=$config_files/hdtn/hdtn_Node2_ltp.json
+sink_config=$config_files/inducts/bpsink_one_stcp_port4558_2NodesTest.json
+
+
+cd $HDTN_SOURCE_ROOT
+
+# GStreamer Pipeline to Save RTP Stream to Disk
+outgoing_rtp_port=8554
+gst-launch-1.0 -e udpsrc address=127.0.0.1 port=8900 ! "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)MP2T, payload=(int)96" \
+        ! rtpmp2tdepay ! queue ! tsparse ! filesink location=received.ts &
+
+sleep 2
+
+gst-launch-1.0 -e udpsrc address=127.0.0.1 port=$outgoing_rtp_port ! "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)MP2T, payload=(int)96" \
+        ! tee name=t \
+        t. ! queue ! udpsink host=127.0.0.1 port=8989 sync=false \
+        t. ! queue ! udpsink host=127.0.0.1 port=8900 sync=false &
+
+sleep 2
+
+# BpReceiveStream
+./build/common/bpcodec/apps/bprecv_stream --my-uri-eid=ipn:2.1 --max-rx-bundle-size-bytes=70000 --num-circular-buffer-vectors=10000 --max-outgoing-rtp-packet-size-bytes=1460 --outduct-type=udp --outgoing-rtp-port=$outgoing_rtp_port --outgoing-rtp-hostname=127.0.0.1 --inducts-config-file=$sink_config &
+
+sleep 3
+
+# HDTN one process
+./build/module/hdtn_one_process/hdtn-one-process --contact-plan-file=$HDTN_SOURCE_ROOT/module/router/contact_plans/contactPlan2Nodes.json --hdtn-config-file=$hdtn_config &
+
+sleep infinity
+
+
+
